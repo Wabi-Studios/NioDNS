5e3b84b3c926e3b578befd69ceeffc7a50cd5fb3
furby™ <devs@wabi.foundation>
2023-03-09 17:58:39 -0600

    Support transport services for cluster on devices.

--- Package.swift
+++ Package.swift
@@ -14,16 +14,23 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.15.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .testTarget(
             name: "DNSClientTests",
-            dependencies: ["DNSClient", "NIO"]),
+            dependencies: [
+              .product(name: "DNSClient"), 
+              .product(name: "NIO", package: "swift-nio")
+            ]),
         .target(
             name: "DNSClient",
-            dependencies: ["NIO"]
+            dependencies: [
+              .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
+              .product(name: "NIO", package: "swift-nio"),
+            ]
         )
     ]
 )
